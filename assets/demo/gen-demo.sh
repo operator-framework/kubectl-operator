b71725181c9fe780468a5cb5791e3a9b231fc3b5
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-22 19:46:24 +0800

    Delete assets/demo directory

--- assets/demo/gen-demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o pipefail
-
-INTERACTIVE=0 asciinema rec --overwrite -c ./assets/demo/demo.sh ./assets/demo/demo.asciinema.json
-asciicast2gif -w 102 -h 34 ./assets/demo/demo.asciinema.json ./assets/demo/demo.gif
-rm ./assets/demo/demo.asciinema.json
