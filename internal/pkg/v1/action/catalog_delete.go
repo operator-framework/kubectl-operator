026eb846800f38b79f8b3e13080d98c40aba8c30
Artur Zych <azych@users.noreply.github.com>
2025-03-06 20:18:36 +0100

    Add command to delete single/all existing olmv1 catalog(s) (#219)
    Signed-off-by: Artur Zych <5843875+azych@users.noreply.github.com>
    Co-authored-by: Artur Zych <5843875+azych@users.noreply.github.com>

---
+++ internal/pkg/v1/action/catalog_delete.go
@@ -0,0 +1,69 @@
+package action
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	olmv1catalogd "github.com/operator-framework/catalogd/api/v1"
+
+	"github.com/operator-framework/kubectl-operator/pkg/action"
+)
+
+type CatalogDelete struct {
+	config      *action.Configuration
+	CatalogName string
+	DeleteAll   bool
+
+	Logf func(string, ...interface{})
+}
+
+func NewCatalogDelete(cfg *action.Configuration) *CatalogDelete {
+	return &CatalogDelete{
+		config: cfg,
+		Logf:   func(string, ...interface{}) {},
+	}
+}
+
+func (cd *CatalogDelete) Run(ctx context.Context) ([]string, error) {
+	// validate
+	if cd.DeleteAll && cd.CatalogName != "" {
+		return nil, errNameAndSelector
+	}
+
+	// delete single, specified catalog
+	if !cd.DeleteAll {
+		return nil, cd.deleteCatalog(ctx, cd.CatalogName)
+	}
+
+	// delete all existing catalogs
+	var catatalogList olmv1catalogd.ClusterCatalogList
+	if err := cd.config.Client.List(ctx, &catatalogList); err != nil {
+		return nil, err
+	}
+	if len(catatalogList.Items) == 0 {
+		return nil, errNoResourcesFound
+	}
+
+	errs := make([]error, 0, len(catatalogList.Items))
+	names := make([]string, 0, len(catatalogList.Items))
+	for _, catalog := range catatalogList.Items {
+		names = append(names, catalog.Name)
+		if err := cd.deleteCatalog(ctx, catalog.Name); err != nil {
+			errs = append(errs, fmt.Errorf("failed deleting catalog %q: %w", catalog.Name, err))
+		}
+	}
+
+	return names, errors.Join(errs...)
+}
+
+func (cd *CatalogDelete) deleteCatalog(ctx context.Context, name string) error {
+	op := &olmv1catalogd.ClusterCatalog{}
+	op.SetName(name)
+
+	if err := cd.config.Client.Delete(ctx, op); err != nil {
+		return err
+	}
+
+	return waitForDeletion(ctx, cd.config.Client, op)
+}
