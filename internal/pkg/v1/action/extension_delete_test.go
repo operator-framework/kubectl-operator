19f30e19fe9d23d55b52f0e73c942b6f8705eeaf
Lalatendu Mohanty <lmohanty@redhat.com>
2025-03-18 17:36:28 -0400

    Code for extension install and delete
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

---
+++ internal/pkg/v1/action/extension_delete_test.go
@@ -0,0 +1,111 @@
+package action_test
+
+import (
+	"context"
+	"slices"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	olmv1 "github.com/operator-framework/operator-controller/api/v1"
+
+	internalaction "github.com/operator-framework/kubectl-operator/internal/pkg/v1/action"
+	"github.com/operator-framework/kubectl-operator/pkg/action"
+)
+
+var _ = Describe("ExtensionDelete", func() {
+	setupEnv := func(extensions ...client.Object) action.Configuration {
+		var cfg action.Configuration
+
+		sch, err := action.NewScheme()
+		Expect(err).To(BeNil())
+
+		cl := fake.NewClientBuilder().
+			WithObjects(extensions...).
+			WithScheme(sch).
+			Build()
+		cfg.Scheme = sch
+		cfg.Client = cl
+
+		return cfg
+	}
+
+	It("fails because of both extension name and --all specifier being present", func() {
+		cfg := setupEnv(setupTestExtensions(2)...)
+
+		deleter := internalaction.NewExtensionDelete(&cfg)
+		deleter.ExtensionName = "foo"
+		deleter.DeleteAll = true
+		extNames, err := deleter.Run(context.TODO())
+		Expect(err).NotTo(BeNil())
+		Expect(extNames).To(BeEmpty())
+
+		validateExistingExtensions(cfg.Client, []string{"ext1", "ext2"})
+	})
+
+	It("fails deleting a non-existent extensions", func() {
+		cfg := setupEnv(setupTestExtensions(2)...)
+
+		deleter := internalaction.NewExtensionDelete(&cfg)
+		deleter.ExtensionName = "does-not-exist"
+		extNames, err := deleter.Run(context.TODO())
+		Expect(err).NotTo(BeNil())
+		Expect(extNames).To(BeEmpty())
+
+		validateExistingExtensions(cfg.Client, []string{"ext1", "ext2"})
+	})
+
+	It("successfully deletes an existing extension", func() {
+		cfg := setupEnv(setupTestExtensions(3)...)
+
+		deleter := internalaction.NewExtensionDelete(&cfg)
+		deleter.ExtensionName = "ext2"
+		_, err := deleter.Run(context.TODO())
+		Expect(err).To(BeNil())
+
+		validateExistingExtensions(cfg.Client, []string{"ext1", "ext3"})
+	})
+
+	It("fails deleting all extensions because there are none", func() {
+		cfg := setupEnv()
+
+		deleter := internalaction.NewExtensionDelete(&cfg)
+		deleter.DeleteAll = true
+		extNames, err := deleter.Run(context.TODO())
+		Expect(err).NotTo(BeNil())
+		Expect(extNames).To(BeEmpty())
+
+		validateExistingExtensions(cfg.Client, []string{})
+	})
+
+	It("successfully deletes all extensions", func() {
+		cfg := setupEnv(setupTestExtensions(3)...)
+
+		deleter := internalaction.NewExtensionDelete(&cfg)
+		deleter.DeleteAll = true
+		extNames, err := deleter.Run(context.TODO())
+		Expect(err).To(BeNil())
+		Expect(extNames).To(ContainElements([]string{"ext1", "ext2", "ext3"}))
+
+		validateExistingExtensions(cfg.Client, []string{})
+	})
+})
+
+// validateExistingExtensions compares the names of the existing extensions with the wanted names
+// and ensures that all wanted names are present in the existing extensions
+func validateExistingExtensions(c client.Client, wantedNames []string) {
+	var extensionList olmv1.ClusterExtensionList
+	err := c.List(context.TODO(), &extensionList)
+	Expect(err).To(BeNil())
+
+	extensions := extensionList.Items
+	Expect(extensions).To(HaveLen(len(wantedNames)))
+	for _, wantedName := range wantedNames {
+		Expect(slices.ContainsFunc(extensions, func(ext olmv1.ClusterExtension) bool {
+			return ext.Name == wantedName
+		})).To(BeTrue())
+	}
+}
--- internal/pkg/v1/action/extension_install.go
+++ internal/pkg/v1/action/extension_install.go
@@ -2,24 +2,33 @@ package action
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	olmv1 "github.com/operator-framework/operator-controller/api/v1"
+	ocv1 "github.com/operator-framework/operator-controller/api/v1"
 
 	"github.com/operator-framework/kubectl-operator/pkg/action"
 )
 
 type ExtensionInstall struct {
-	config *action.Configuration
-
-	Package string
-
-	Logf func(string, ...interface{})
+	config         *action.Configuration
+	ExtensionName  string
+	Namespace      NamespaceConfig
+	PackageName    string
+	Channels       []string
+	Version        string
+	ServiceAccount string
+	CleanupTimeout time.Duration
+	Logf           func(string, ...interface{})
+}
+type NamespaceConfig struct {
+	Name string
 }
 
 func NewExtensionInstall(cfg *action.Configuration) *ExtensionInstall {
@@ -29,47 +38,93 @@ func NewExtensionInstall(cfg *action.Configuration) *ExtensionInstall {
 	}
 }
 
-func (i *ExtensionInstall) Run(ctx context.Context) (*olmv1.ClusterExtension, error) {
-	// TODO(developer): Lookup package information when the OLMv1 equivalent of the
-	//     packagemanifests API is available. That way, we can check to see if the
-	//     package is actually available to the cluster before creating the Extension
-	//     object.
-
-	opKey := types.NamespacedName{Name: i.Package}
-	op := &olmv1.ClusterExtension{
-		ObjectMeta: metav1.ObjectMeta{Name: opKey.Name},
-		Spec: olmv1.ClusterExtensionSpec{
-			Source: olmv1.SourceConfig{
-				SourceType: "Catalog",
-				Catalog: &olmv1.CatalogFilter{
-					PackageName: i.Package,
+func (i *ExtensionInstall) buildClusterExtension() ocv1.ClusterExtension {
+	extension := ocv1.ClusterExtension{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: i.ExtensionName,
+		},
+		Spec: ocv1.ClusterExtensionSpec{
+			Source: ocv1.SourceConfig{
+				SourceType: ocv1.SourceTypeCatalog,
+				Catalog: &ocv1.CatalogFilter{
+					PackageName: i.PackageName,
+					Version:     i.Version,
 				},
+			},
+			Namespace: i.Namespace.Name,
+			ServiceAccount: ocv1.ServiceAccountReference{
+				Name: i.ServiceAccount,
 			},
 		},
 	}
-	if err := i.config.Client.Create(ctx, op); err != nil {
+
+	return extension
+}
+
+func (i *ExtensionInstall) Run(ctx context.Context) (*ocv1.ClusterExtension, error) {
+	extension := i.buildClusterExtension()
+
+	// Add Channels to extension
+	if len(i.Channels) > 0 {
+		extension.Spec.Source.Catalog.Channels = i.Channels
+	}
+
+	// TODO: Add CatalogSelector to extension
+
+	// Create the extension
+	if err := i.config.Client.Create(ctx, &extension); err != nil {
 		return nil, err
 	}
+	clusterExtension, err := i.waitForExtensionInstall(ctx)
+	if err != nil {
+		cleanupCtx, cancelCleanup := context.WithTimeout(context.Background(), i.CleanupTimeout)
+		defer cancelCleanup()
+		cleanupErr := i.cleanup(cleanupCtx)
+		return nil, errors.Join(err, cleanupErr)
+	}
+	return clusterExtension, nil
+}
 
-	// TODO(developer): Improve the logic in this poll wait once the Extension reconciler
-	//     and conditions types and reasons are improved. For now, this will stop waiting as
-	//     soon as a Ready condition is found, but we should probably wait until the Extension
-	//     stops progressing.
-	// All Types will exist, so Ready may have a false Status. So, wait until
-	// Type=Ready,Status=True happens
-
-	if err := wait.PollUntilContextCancel(ctx, pollInterval, true, func(conditionCtx context.Context) (bool, error) {
-		if err := i.config.Client.Get(conditionCtx, opKey, op); err != nil {
+// waitForClusterExtensionInstalled waits for the ClusterExtension to be installed
+// and returns the ClusterExtension object
+func (i *ExtensionInstall) waitForExtensionInstall(ctx context.Context) (*ocv1.ClusterExtension, error) {
+	clusterExtension := &ocv1.ClusterExtension{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: i.ExtensionName,
+		},
+	}
+	errMsg := ""
+	key := client.ObjectKeyFromObject(clusterExtension)
+	if err := wait.PollUntilContextCancel(ctx, time.Millisecond*250, true, func(conditionCtx context.Context) (bool, error) {
+		if err := i.config.Client.Get(conditionCtx, key, clusterExtension); err != nil {
 			return false, err
 		}
-		installedCondition := meta.FindStatusCondition(op.Status.Conditions, olmv1.TypeInstalled)
-		if installedCondition != nil && installedCondition.Status == metav1.ConditionTrue {
-			return true, nil
+		progressingCondition := meta.FindStatusCondition(clusterExtension.Status.Conditions, ocv1.TypeProgressing)
+		if progressingCondition != nil && progressingCondition.Reason != ocv1.ReasonSucceeded {
+			errMsg = progressingCondition.Message
+			return false, nil
 		}
-		return false, nil
+		if !meta.IsStatusConditionPresentAndEqual(clusterExtension.Status.Conditions, ocv1.TypeInstalled, metav1.ConditionTrue) {
+			return false, nil
+		}
+		return true, nil
 	}); err != nil {
-		return nil, fmt.Errorf("waiting for extension to become ready: %v", err)
+		if errMsg == "" {
+			errMsg = err.Error()
+		}
+		return nil, fmt.Errorf("cluster extension %q did not finish installing: %s", clusterExtension.Name, errMsg)
 	}
+	return clusterExtension, nil
+}
 
-	return op, nil
+func (i *ExtensionInstall) cleanup(ctx context.Context) error {
+	clusterExtension := &ocv1.ClusterExtension{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: i.ExtensionName,
+		},
+	}
+	if err := waitForDeletion(ctx, i.config.Client, clusterExtension); err != nil {
+		return fmt.Errorf("delete clusterextension %q: %v", i.ExtensionName, err)
+	}
+	return nil
 }
